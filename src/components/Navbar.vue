2f71fa0f9e3dc1fca76676f33f64443ce13e384f
Karl Yang <ccok12345678@gmail.com>
2022-02-10 14:38:05 +0800

    added navbar brand

---
+++ src/components/Navbar.vue
@@ -0,0 +1,31 @@
+<template lang="pug">
+nav.navbar.navbar-expand-lg.navbar-light.bg-light
+  .container
+
+    a.navbar-brand(href='#')
+      h1.visually-hidden The F2E 3rd 前端薪資數據圖表調查
+      img(src='../assets/images/The_F2E.svg'
+        title='The F2E 3rd 前端薪資數據圖表調查'
+        alt='The F2E 3rd')
+
+    button.navbar-toggler(
+      type='button'
+      data-bs-toggle='collapse'
+      data-bs-target='#navbarNav'
+      aria-controls='navbarNav'
+      aria-expanded='false'
+      aria-label='Toggle navigation')
+      span.navbar-toggler-icon
+
+    #navbarNav.collapse.navbar-collapse
+      ul.navbar-nav
+        li.nav-item
+          a.nav-link.active(aria-current='page' href='#') Home
+        li.nav-item
+          a.nav-link(href='#') Features
+        li.nav-item
+          a.nav-link(href='#') Pricing
+        li.nav-item
+          a.nav-link.disabled Disabled
+
+</template>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,14 @@
-<template>
-  <div class="home">
-  </div>
+<template lang="pug">
+Navbar
 </template>
 
 <script>
+import Navbar from '@/components/Navbar.vue';
 
 export default {
   name: 'Home',
-
+  components: {
+    Navbar,
+  },
 };
 </script>
