a694b061d65fc3d9ab7969b2fa15edc84e799036
Karl Yang <ccok12345678@gmail.com>
2022-02-10 22:38:13 +0800

    added router-link

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -29,16 +29,20 @@ nav.navbar.navbar-expand-md.navbar-light.bg-trasparent.py-4
           a#navDropBtn.nav-link.text-lighter.dropdown-toggle(
             href='#'
             data-bs-toggle='dropdown'
-            ) 薪資調查報告
+          ) 薪資調查報告
 
           ul.dropdown-menu.dropdown-menu-dark.border-lighter.py-1.pe-1(
             arial-labelledby='navDropBtn')
             li.p-2
-              a.dropdown-item.py-2.pe-5.me-3 前端薪資調查報告
+              router-link.dropdown-item.py-2.pe-5.me-3(
+                to='/前端薪資調查報告'
+              ) 前端薪資調查報告
             li
               hr.dropdown-devider.m-0
             li.p-2
-              a.dropdown-item.py-2 UI 薪資調查報告
+              router-link.dropdown-item.py-2(
+                to='/UI 薪資調查報告'
+              ) UI 薪資調查報告
 
         li.nav-item.ms-md-auto
           a.nav-link.text-primary(href='#') Login
