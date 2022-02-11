8f0fc5b397d5882c854f4145d6f24ab1131cebf4
Karl Yang <ccok12345678@gmail.com>
2022-02-11 14:47:36 +0800

    added dropdown

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,21 +1,39 @@
 <template lang="pug">
-head.conteiner.d-flex.flex-column.align-items-center
+head.container.d-flex.flex-column.align-items-center
 
   h2.fw-bold.pb-2 {{ Heading }}
 
-  .switch.mt-5.p-2.border.border-lighter.rounded
+  .row.switch.mt-5.w-100
 
-    button.fw-bold.btn.py-2.px-4.me-2(
-      :class='{ "btn-switched": isBasic }'
-      @click='isBasic = true'
-    )
-      | 基本資料
+    .col-12.col-sm-8.offset-md-2.col-md-6.offset-lg-4.col-lg-4
 
-    button.fw-bold.btn.py-2.px-4(
-      :class='{ "btn-switched": !isBasic }'
-      @click='isBasic = false'
-    )
-      | 薪資與產業
+      .p-2.border.border-lighter.rounded.d-flex.justify-content-around
+
+        button.fw-bold.btn.py-2.px-4.me-2(
+          :class='{ "btn-switched": isBasic }'
+          @click='isBasic = true'
+        )
+          | 基本資料
+
+        button.fw-bold.btn.py-2.px-4(
+          :class='{ "btn-switched": !isBasic }'
+          @click='isBasic = false'
+        )
+          | 薪資與產業
+
+    .dropdown.col-12.col-sm-4.text-center.text-sm-end.mt-3.mt-sm-0
+      a#headDropBtn.dropdown-toggle.btn.bg-darker.text-lighter.px-4.py-3(
+        href='#'
+        data-bs-toggle='dropdown'
+      )
+        span.me-5 2021
+
+      ul.dropdown-menu.dropdown-menu-dark(
+        arial-labelledby='headDropBtn')
+        li
+          a.dropdown-item(
+            href='#'
+          ) 2021
 
 </template>
 
