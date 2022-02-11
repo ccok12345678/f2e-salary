2a8ee7a82297d526b1e7c1158a5df791043d6401
Karl Yang <ccok12345678@gmail.com>
2022-02-11 14:51:13 +0800

    added Header

--- src/views/DesignerCharts.vue
+++ src/views/DesignerCharts.vue
@@ -1,3 +1,22 @@
 <template lang="pug">
-h2 Designer
+Header(:heading='heading')
 </template>
+
+<script>
+import { ref } from 'vue';
+import Header from '../components/Header.vue';
+
+export default {
+  components: {
+    Header,
+  },
+
+  setup() {
+    const heading = ref('UI 薪資調查報告');
+
+    return {
+      heading,
+    };
+  },
+};
+</script>
