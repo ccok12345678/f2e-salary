e6794a1fe87f5121b193a486f2e3142c08a26639
Karl Yang <ccok12345678@gmail.com>
2022-02-13 10:38:55 +0800

    added ChartAge

---
+++ src/components/Chart_Age.vue
@@ -0,0 +1,94 @@
+<template lang="pug">
+section.chart
+
+  header.chart-header
+
+    h4.chart-title
+      | 年齡
+
+  main.chart-body
+
+    small.chart-unit
+      | 單位：人
+
+    BarChart.chart-content(
+      :chartData='chartData' :options='options')
+
+</template>
+
+<script>
+import { ref } from 'vue';
+import { useRoute } from 'vue-router';
+import { BarChart } from 'vue-chart-3';
+
+export default {
+  name: 'Home',
+  components: { BarChart },
+  setup() {
+    // get data
+    const route = useRoute();
+    const api = (route.name === 'Designer')
+      ? 'https://raw.githubusercontent.com/hexschool/2021-ui-frontend-job/master/ui_data.json'
+      : 'https://raw.githubusercontent.com/hexschool/2021-ui-frontend-job/master/frontend_data.json';
+
+    const areas = ref([]);
+    const counts = ref([]);
+
+    fetch(api)
+      .then(async (fetchData) => {
+        const rawData = await fetchData.json();
+
+        rawData.forEach((people) => {
+          let area = people.company.area.replace('台灣 - ', '');
+          area = area.replace('(綠島、澎湖、金門、馬祖)', '');
+          if (!areas.value.includes(area)) {
+            areas.value.push(area);
+            counts.value[areas.value.indexOf(area)] = 1;
+          } else {
+            counts.value[areas.value.indexOf(area)] += 1;
+          }
+        });
+      });
+
+    // chart
+    const chartData = {
+      labels: areas.value,
+      datasets: [{
+        data: counts.value,
+      }],
+    };
+
+    const options = {
+      scales: {
+        x: {
+          grid: {
+            display: false,
+          },
+          ticks: {
+            autoSkip: false,
+            maxRotation: 0,
+          },
+        },
+        y: {
+          grid: {
+            drawTicks: true,
+            color: '#6B6783',
+            tickColor: '#080231',
+            tickLength: 24,
+          },
+          min: 0,
+          max: 350,
+          ticks: {
+            stepSize: 50,
+          },
+        },
+      },
+    };
+
+    return {
+      chartData,
+      options,
+    };
+  },
+};
+</script>
--- src/views/BasicCharts.vue
+++ src/views/BasicCharts.vue
@@ -4,15 +4,19 @@
 
     .col-md-6
       ChartAreas
+
     .col-md-6
+      ChartAge
 </template>
 
 <script>
 import ChartAreas from '@/components/Chart_Areas.vue';
+import ChartAge from '@/components/Chart_Age.vue';
 
 export default {
   components: {
     ChartAreas,
+    ChartAge,
   },
 };
 </script>
