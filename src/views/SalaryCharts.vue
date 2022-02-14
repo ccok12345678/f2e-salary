cf3cb13c6ef0223a6263c9ddb767b8cb5d74b7fa
Karl Yang <ccok12345678@gmail.com>
2022-02-14 15:40:14 +0800

    added ChartJobTenure

--- src/views/SalaryCharts.vue
+++ src/views/SalaryCharts.vue
@@ -1,3 +1,15 @@
 <template lang="pug">
-h2 薪資與產業
+.container.pb-1
+  ChartJobTenure
+
 </template>
+
+<script>
+import ChartJobTenure from '@/components/Chart_JobTenure.vue';
+
+export default {
+  components: {
+    ChartJobTenure,
+  },
+};
+</script>
