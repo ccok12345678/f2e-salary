fdc8baef15779426c9220d9415b58b65f2dddbe2
Karl Yang <ccok12345678@gmail.com>
2022-02-18 21:01:09 +0800

    added chart data

--- src/components/ChartIndustry.vue
+++ src/components/ChartIndustry.vue
@@ -11,43 +11,34 @@ section.chart
     .col-md-6
       .row.g-0.w-100
 
-        .col-3.d-flex.align-items-center.justify-content-center(
+        .col-4.d-flex.align-items-center.justify-content-end(
           v-if="percentage.length !== 0"
         )
           .fw-bold.d-flex.flex-column.align-items-center
-            span {{ percentage[1].gender }}
-            span  {{ percentage[1].percent }} %
+            span {{ percentage[1].label }}
+            span  {{ percentage[1].percentage }} %
 
-        PieChart.chart-content.col-6(
+        PieChart.chart-content.col-4.offset-1(
           :chartData='chartData')
 
-        .col-3.d-flex.align-items-center.justify-content-center(
+        .col-2.offset-1.d-flex.align-items-center(
           v-if="percentage.length !== 0"
         )
           .fw-bold.d-flex.flex-column.align-items-center
-            span {{ percentage[0].gender }}
-            span {{ 100 - percentage[1].percent }} %
+            span {{ percentage[0].label }}
+            span {{ 100 - percentage[1].percentage }} %
+
     .col-md-6.d-flex.justify-content-center.align-items-center
-        .chart-card.py-4.bg-dark.rounded
+      .chart-card.py-4.bg-dark.rounded
 
-          //- for engineer
-          .px-3.mx-3(v-if='$route.name !== "Designer"')
-            h6.fw-bold 後端語言
-            p.text-light.pb-4.mb-3 Node.js、Python、PHP、JAVA、Ruby
-            h6.fw-bold 任務指派工具
-            p.text-light.pb-4.mb-3 Trello、asana
-            h6.fw-bold UI 標示工具
-            p.text-light.mb-0 Adobe XD、Figma
-
-          //- for designer
-          .px-3.mx-3(v-else)
-            h6.fw-bold 應用軟體
-            p.text-light.pb-4.mb-3 Adobe XD、Photoshop、Illustrator、Figma
+        //- for engineer
+        .px-3.mx-3
+          h6.fw-bold 自有產品公司之產業人數排名 Top 10
+          ul.text-light.d-flex.flex-wrap.row-cols-2
+            li.col-6(v-for="(industry, key) in top10" :key="key")
+              | {{ industry.category}}
 
 </template>
-
-<style lang="scss" scoped>
-</style>
 
 <script>
 import { ref } from 'vue';
@@ -64,35 +55,71 @@ export default {
   setup(props) {
     const apiData = ref(props.apiData);
 
-    const genders = ref([]);
-    const counts = ref([]);
-    const percentage = ref([]);
+    const labels = ['接案公司', '自有產品'];
+    const target = '接案公司';
+    const targetRex = new RegExp(target);
+
+    const industries = [];
+    const industryCount = [];
+
+    const top10 = ref([]);
+    const data = ref(labels.map(() => 0));
+    const percentage = ref(labels.map((label) => ({
+      label,
+      percentage: 0,
+    })));
 
     apiData.value
       .then((rawData) => {
         rawData.forEach((people) => {
-          const { gender } = people;
-          if (!genders.value.includes(gender)) {
-            genders.value.push(gender);
-            counts.value[genders.value.indexOf(gender)] = 1;
-            percentage.value.push({
-              gender: gender.replace('性', ''),
-              percent: 0,
-            });
+          const { industry } = people.company;
+
+          // chart data
+          if (targetRex.test(industry)) {
+            data.value[labels.indexOf(industry)] += 1;
           } else {
-            counts.value[genders.value.indexOf(gender)] += 1;
-            const total = counts.value.reduce((x, y) => x + y);
-            const percent = Math.round((counts.value[genders.value.indexOf(gender)] / total) * 100);
-            percentage.value[genders.value.indexOf(gender)].percent = percent;
+            data.value[1] += 1;
+
+            //  top 10
+            if (!industries.includes(industry)) {
+              industries.push(industry);
+              industryCount.push({
+                category: industry,
+                count: 1,
+              });
+            } else {
+              industryCount[industries.indexOf(industry)].count += 1;
+            }
           }
+        });
+        // percentage
+        percentage.value[0].percentage = Math.round((data.value[0] / rawData.length) * 100);
+        percentage.value[1].percentage = 100 - percentage.value[0].percentage;
+
+        // top 10, bubble sort
+        const len = industryCount.length;
+        for (let i = 0; i < len - 1; i += 1) {
+          for (let j = 0; j < len - 1 - i; j += 1) {
+            if (industryCount[j].count < industryCount[j + 1].count) {
+              const temp = industryCount[j];
+              industryCount[j] = industryCount[j + 1];
+              industryCount[j + 1] = temp;
+            }
+          }
+        }
+
+        industryCount.every((industry, index) => {
+          top10.value.push(industry);
+          return index < 9;
         });
       });
 
     // chart
     const chartData = {
-      labels: genders.value,
+      labels,
+      responsive: true,
       datasets: [{
-        data: counts.value,
+        data: data.value,
         backgroundColor: [
           '#8E7DFA',
           '#D2CBFD',
@@ -103,7 +130,14 @@ export default {
     return {
       chartData,
       percentage,
+      top10,
     };
   },
 };
 </script>
+
+<style lang="scss">
+.chart-card {
+  max-width: 325px;
+}
+</style>
